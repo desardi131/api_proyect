8ff841f8ab86a7672dc8528fe072d54c76f78cc8
desardi131 <desardi131@gmail.com>
2022-12-17 16:13:25 +0100

    commit old files

---
+++ api_proyecto.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request,redirect, url_for, flash, jsonify
+import pickle
+import numpy as np
+
+# model = pickle.load(open('iri.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('home_app.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/after_predict', methods=['POST'])
+def after_predict():
+    return render_template('after_predict.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
