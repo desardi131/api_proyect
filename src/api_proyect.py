e1c5ced88f0d7acf4d253105020d1deea3f3d095
LLBF <ll.balsecawork@gmail.com>
2022-12-18 22:10:07 +0100

    ruta update

--- src/api_proyect.py
+++ src/api_proyect.py
@@ -1,16 +1,47 @@
 from flask import Flask, render_template, request,redirect, url_for, flash, jsonify
 import pickle
 import numpy as np
-
+import pandas as pd
+import pymysql
 # model = pickle.load(open('iri.pkl', 'rb'))
 
-app = Flask(__name__) 
-
+app = Flask(__name__)
 
 
 @app.route('/')
 def man():
     return render_template('home_app.html')
+
+# Esto es sólo pa subir los últimos 30 días de la base de datos
+@app.route('/update', methods=["GET"])
+def get_all():
+    username = "DataScienceTB22"
+    password = "pyquirrinds"
+    host = "database-1.ceaq9kvyuwau.us-east-2.rds.amazonaws.com"
+    port = "3306"
+    cursorclass = pymysql.cursors.DictCursor
+
+    # gets the credentials from .aws/credentials
+    db = pymysql.connect(host=host,
+                         user=username,
+                         password=password,
+                         cursorclass=cursorclass,
+                         )
+
+    cursor = db.cursor()
+
+    cursor.connection.commit()
+    use_db = ''' USE users_web'''
+    cursor.execute(use_db)
+
+    sql = '''SELECT * FROM users'''
+    cursor.execute(sql)
+    tabla = cursor.fetchall()
+    tabla = pd.DataFrame(tabla)
+    tabla = tabla[-30:]
+    db.close()
+    return render_template('update.html', tables=[tabla.to_html()])
+
 
 @app.route('/predict')
 def predict():
