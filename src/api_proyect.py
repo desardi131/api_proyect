b713e7bf0b773cfc74ff54b331b87e400e4745c0
axel8verde <alejandro.sanchez.amor@gmail.com>
2022-12-18 23:00:29 +0100

    Nuevos Cambios en la api y todos los templates

--- src/api_proyect.py
+++ src/api_proyect.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 def man():
     return render_template('home_app.html')
 
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.route('/re_train')
+def re_train():
+    return render_template('re_train.html')
+
 @app.route('/predict')
 def predict():
     return render_template('predict.html')
